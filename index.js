a9659f0ada0eea9a30b48184e550fbc072d6afa1
Brandon <90708663+BrandonW1998@users.noreply.github.com>
2024-07-08 20:32:26 -0400

    moved collision handling to index

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+const collisionBlocks = [];
+
+const parsedCollisions = collisionsLevel1.parse2D();
+parsedCollisions.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 292) {
+      // push new collision into collisionBlocks array
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 64,
+            y: y * 64,
+          },
+        })
+      );
+    }
+  });
+});
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -37,6 +56,9 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   backgroundLevel1.draw();
+  collisionBlocks.forEach((CollisionBlock) => {
+    CollisionBlock.draw();
+  });
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
