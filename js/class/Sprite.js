0a168a9b7fd1f9081b94f1ebd8e3c88b278f0818
Brandon <90708663+BrandonW1998@users.noreply.github.com>
2024-08-22 13:16:42 -0400

    removed image.onLoad, wasn't firing properly

--- js/class/Sprite.js
+++ js/class/Sprite.js
@@ -2,14 +2,20 @@ class Sprite {
   constructor({ position, imageSrc }) {
     this.position = position;
     this.image = new Image();
-    this.image.onLoad = () => {
-      this.loaded = true;
-    };
+    // image.onLoad leading to issues, not needed as of now
+    // this.image.onLoad = () => {
+    //   console.log("loaded");
+    //   this.loaded = true;
+    //   this.width = this.image.width / 11;
+    //   this.height = this.image.height;
+    // };
+    this.width = this.image.width / 11;
+    this.height = this.image.height;
     this.image.src = imageSrc;
-    this.loaded = false;
+    this.loaded = true;
   }
   draw() {
-    if (this.loaded) return;
+    if (!this.loaded) return;
     c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
