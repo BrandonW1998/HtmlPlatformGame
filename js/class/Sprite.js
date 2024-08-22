75d5cff7c002b1d4abf9f29585cbaaef94811411
Brandon <90708663+BrandonW1998@users.noreply.github.com>
2024-08-22 14:27:44 -0400

    fixed image.onload issue, onLoad -> onload

--- js/class/Sprite.js
+++ js/class/Sprite.js
@@ -2,17 +2,13 @@ class Sprite {
   constructor({ position, imageSrc }) {
     this.position = position;
     this.image = new Image();
-    // image.onLoad leading to issues, not needed as of now
-    // this.image.onLoad = () => {
-    //   console.log("loaded");
-    //   this.loaded = true;
-    //   this.width = this.image.width / 11;
-    //   this.height = this.image.height;
-    // };
-    this.width = this.image.width / 11;
-    this.height = this.image.height;
+    this.image.onload = () => {
+      this.loaded = true;
+      this.width = this.image.width / 11;
+      this.height = this.image.height;
+    };
     this.image.src = imageSrc;
-    this.loaded = true;
+    this.loaded = false;
   }
   draw() {
     if (!this.loaded) return;
